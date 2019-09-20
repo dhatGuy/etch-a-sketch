1698cf19101abf6f67000653de9d6c53215006e0
Odunsi Joseph <odunsiolakunbi@gmail.com>
2019-09-20 22:56:39 +0100

    create grid

---
+++ script.js
@@ -0,0 +1,24 @@
+const item = document.querySelector('body');
+const container = document.querySelector('.container');
+const containerStyle = getComputedStyle(container);
+const containerHeight = Number(containerStyle.height.replace('px', ""));
+const containerWidth = Number(containerStyle.width.replace('px', ""))
+console.clear()
+console.log(containerHeight)
+
+const createDiv = (num) =>{
+    for(i = 0; i < num; i++){
+        const row = document.createElement('div');
+        container.appendChild(row);
+        for(j = 0; j < num; j++){
+            const box = document.createElement('div');
+            box.innerHTML = j;
+            box.style.width = (containerWidth/num) + 'px';
+            box.style.height = (containerHeight/num)  + 'px';
+            box.className = 'item';
+            row.appendChild(box);
+        }
+    }
+}
+
+createDiv(20)
